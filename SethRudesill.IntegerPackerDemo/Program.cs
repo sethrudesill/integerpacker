83d96d5d16bd0ccc2726183c8f017543c412643d
Seth Rudesill <sethrudesill@gmail.com>
2017-02-18 11:54:02 -0500

    Initial demo program in .NET core which takes two 16-bit integers and packs them into one 32-bit integer.

---
+++ SethRudesill.IntegerPackerDemo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SethRudesill.IntegerPackerDemo
+{
+    class Program
+    {
+        private static readonly string Prompt1 = $"Left integer between 1 and {UInt16.MaxValue}: ";
+        private static readonly string Prompt2 = $"Right integer between 1 and {UInt16.MaxValue}: ";
+        private static readonly string Prompt3 = "Try again? Y/N: ";
+        private static volatile string Input;
+        private static UInt16 Left;
+        private static UInt16 Right;
+        private static UInt32 Result;
+
+        static Program()
+        {
+            Console.Title = "Integer Packer Demo";
+        }
+
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.Clear();
+                Console.Write(Prompt1);
+                Input = Console.ReadLine();
+                if (UInt16.TryParse(Input, out Left))
+                {
+                    Console.WriteLine();
+                    Console.Write(Prompt2);
+                    Input = Console.ReadLine();
+                    if (UInt16.TryParse(Input, out Right))
+                    {
+                        Console.WriteLine();
+                        Result = (UInt32) Left << 16 | Right;
+                        Console.WriteLine("The result is {0}", Result);
+                        Console.Write(Prompt3);
+                        if (Console.ReadKey().Key.Equals(ConsoleKey.Y) == false)
+                            break;
+                    }
+                }
+            } while (true);
+        }
+    }
+}
